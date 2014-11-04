e11684aef5e0d481c0eab0a5dd25d765b42a790f
ArtyomShaitor <artyomshaitor@gmail.com>
2014-11-04 11:30:52 +0400

    Create Project
    Java application was created

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        char ch;
+        try{
+            ch = (char)System.in.read();
+        }catch(IOException e){
+
+        }
+
+    }
+}
